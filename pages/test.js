1d82e33518d3341e022bbc932c0a576df0757abc
Olivier <olivier@dewereldvanolivier.nl>
2023-05-26 22:36:19 +0200

    fixed big bug by taking score and input out useEffect

--- pages/test.js
+++ pages/test.js
@@ -18,8 +18,8 @@ export default function labelMachine({size=150}) {
   const [ID,setID] = useState("")
   const [countDown, setCountDown] = useState(3);
 
+  
   const correctLabel = correctLabels[index]
- 
 
   const keyPress = useCallback((e) => {
     let k=e.key
@@ -37,57 +37,31 @@ export default function labelMachine({size=150}) {
   useEffect(() => { 
     // add and remove event listener
    document.addEventListener('keydown', keyPress);
-   return () => {
-    document.removeEventListener('keydown', keyPress);
-   };
+   //return () => {
+   //  document.removeEventListener('keydown', keyPress);
+   //};
   }, [keyPress]);
 
  
   
   
   useEffect(() => {
-
    
-   
-    
     const interval = setInterval(() => {
      
       if (stop === 1)
     {
       setCountDown((cD)=> cD-1);
       if (countDown==0){
-        collectLabels()
         console.timeEnd("plaatje tijd");
         selectRandomImage();
         console.time("plaatje tijd");
         setCountDown(0);
         setCounter((c)=> c+1);
-        console.log(input)
-        setCenterColor(styles.centerImageBlack);
-        if(input!=correctLabel){
-          setScore(score+1)
-          setCenterColor(styles.centerImageRed)
-        }
-        if (input==correctLabel){
-              setScore(score-1)
-              if (score < 0)
-              {  
-                setScore(0)
-              }
-              setCenterColor(styles.centerImageBlue)
-            }
-    checkSpeed()
-     
+        setCenterColor(styles.centerImageBlack)
         
-        if (speed>500){
-          setSpeed(speed -5)
-        }
-      
-        if (score>6){
-          setSpeed(1200)
-          setScore(3)
-        }  
-    }
+       
+      }
 
      }
      else {
@@ -95,7 +69,7 @@ export default function labelMachine({size=150}) {
      }
     }, speed);
     return () => clearInterval(interval);
-  }, [stop,speed,counter,countDown,score,correctLabel,input]);
+  }, [stop,speed,counter,countDown]);
   
   const fetchData = async () => {
     const response = await fetch('/api/labels')
@@ -115,36 +89,6 @@ export default function labelMachine({size=150}) {
       console.log(data);
   }
 
-  function checkSpeed(){
-    if(input == 's'){
-      setStop(1)
-      }
-    else if(input == 'p'){
-        setStop(0)
-      }
-    else if(input == 'u'){
-      if (speed > 100){
-        setSpeed(speed-100)
-      }
-    }
-    else if(input== 'd'){
-        setSpeed(speed+100);
-        }
-    else if(input!=correctLabel){
-     
-      setScore(score+1)
-    
-      setCenterColor(styles.centerImageRed)
-    }
-    else if (input==correctLabel){
-      setScore(score-1)
-      if (score < 0)
-      {  
-        setScore(0)
-      }
-      setCenterColor(styles.centerImageBlue)}
-    }
-
   function StartStopMenu(){
     if (countDown==0){
         return (
@@ -162,26 +106,53 @@ export default function labelMachine({size=150}) {
         </ul>);
   }
 
-  function placeCursor(){
-    const input = document.querySelector("input");
-    input.focus();
-    input.select();
-  }
-
-
-function start(){
-    setID(window.prompt("What is your name?"))
-    // placeCursor()
-    setStop(1)
-    
-  }
 
 function selectRandomImage(){
   let rand = Math.floor(Math.random() * 60000);
   setIndex(rand)
 }
 
+function checkSpeed(){
+if(input == 's'){
+  setStop(1)
+  }
+else if(input == 'p'){
+    setStop(0)
+  }
+else if(input == 'u'){
+  if (speed > 100){
+    setSpeed(speed-100)
+  }
+}
+else if(input== 'd'){
+    setSpeed(speed+100);
+    }
+else if(input!=correctLabel){
+ 
+  setScore(score+1)
 
+  setCenterColor(styles.centerImageRed)
+}
+else if (input==correctLabel){
+  setScore(score-1)
+  if (score < 0)
+  {  
+    setScore(0)
+  }
+  setCenterColor(styles.centerImageBlue)
+}
+
+else if (speed>500){
+  setSpeed(speed -5)
+}
+
+else if (score>6){
+  setSpeed(1200)
+  setScore(3)
+}  
+
+
+}
 
 function collectLabels(){
   setAllLabels([allLabels,input]);
@@ -205,18 +176,13 @@ function collectLabels(){
  </center>
    
   <hr />
-   
-{/* <label>
-        <input value={input} readOnly
-    
+  
+    <label>
+        <input value={input}
+        onInput={checkSpeed}
+        onChange={collectLabels}
         />
-    </label>  */}
-    <hr />
-         
-    <button onClick={start}>START</button>
-          
-
-   
+    </label>
   
 <StartStopMenu />
        <p>f=5, h=6</p>
