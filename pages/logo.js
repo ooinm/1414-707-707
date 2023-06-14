f8b07b4534931a12d231649f115c3bab888c4535
olaf booij thuis <olafbooij@bestprima.nl>
2023-06-14 14:31:08 +0200

    cooler logo : - )

--- pages/logo.js
+++ pages/logo.js
@@ -4,27 +4,23 @@ import styles from '../styles/Home.module.css';
 
 export default function Logo() {
 
-
-  
   return (
 
-  
     <h1 className={styles.title}>
-    
-    <Image src='/logo/1.png' width='25' height='25' alt="1" />
-    <Image src='/logo/4.png' width='25' height='25' alt="4" />
-    <Image src='/logo/1.png' width='25' height='25' alt="1" />
-    <Image src='/logo/4.png' width='25' height='25' alt="4" />
-    =    
-    <Image src='/logo/7.png' width='25' height='25' alt="7" /> 
-    <Image src='/logo/0.png' width='25' height='25' alt="0" /> 
-    <Image src='/logo/7.png' width='25' height='25' alt="7" /> 
-    + 
-    <Image src='/logo/7.png' width='25' height='25' alt="7" /> 
-    <Image src='/logo/0.png' width='25' height='25' alt="0" /> 
-    <Image src='/logo/7.png' width='25' height='25' alt="7" /> 
-    </h1>
 
+    <Image src='/images/416.png' width='28' height='25' alt="1" />
+    <Image src='/images/275.png' width='25' height='25' alt="4" />
+    <Image src='/images/224.png' width='25' height='25' alt="1" />
+    <Image src='/images/412.png' width='25' height='25' alt="4" />
+    =
+    <Image src='/images/168.png' width='25' height='25' alt="7" />
+    <Image src='/images/69.png'  width='25' height='25' alt="0" />
+    <Image src='/images/446.png' width='25' height='25' alt="7" />
+    +
+    <Image src='/images/370.png' width='25' height='25' alt="7" />
+    <Image src='/images/440.png' width='25' height='25' alt="0" />
+    <Image src='/images/340.png' width='25' height='25' alt="7" />
+    </h1>
 
   )
 }
